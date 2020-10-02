00817370cc9e9ca453dcae81c8477baf4ab80db7
mnpenchev <mnpenchev@gmail.com>
2020-10-02 10:45:31 +0100

    added serializer, admin panel for documents

--- DocManSys_django_rest/settings.py
+++ DocManSys_django_rest/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'documents',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'DocManSys_django_rest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'DocManSysDB',
+        'USER': 'postgres',
+        'PASSWORD': 'password1',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- documents/models.py
+++ documents/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+# Create module for documents
+class Document(models.Model):
+    address = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    body = models.CharField(max_length=2000)
+    date = models.DateField()
+    # signature = models.ImageField()
+    signed = models.CharField(max_length=100)
+    status = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    # notes = models.CharField(max_length=400)
+
+
+# return the address and the title in the admin panel Documents screen
+    def __str__(self):
+        return self.address, self.title
