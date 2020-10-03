44fbffbc00020c46971808b2f1f60ec90da5bddd
mnpenchev <mnpenchev@gmail.com>
2020-10-03 08:48:41 +0100

    class based api views

--- documents/urls.py
+++ documents/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import document_list, document_detail
+from .views import document_list, document_detail, DocumentAPIView, DocumentDetails
 
 urlpatterns = [
-    path('document/', document_list),
-    path('document/<int:pk>/', document_detail),
+    # path('document/', document_list),
+    path('document/', DocumentAPIView.as_view()),
+    # path('document/<int:pk>/', document_detail),
+    path('document/<int:id>/', DocumentDetails.as_view()),
 ]
--- documents/views.py
+++ documents/views.py
@@ -6,7 +6,52 @@ from .serializers import DocumentSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
 # Create your views here.
+
+
+class DocumentAPIView(APIView):
+    def get(self, request):
+        documents = Document.objects.all()
+        serializer = DocumentSerializer(documents, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = DocumentSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class DocumentDetails(APIView):
+    def get_object(self, id):
+        try:
+            return Document.objects.get(id=id)
+        except Document.DoesNotExist:
+            return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, id):
+        document = self.get_object(id)
+        serializer = DocumentSerializer(document)
+        return Response(serializer.data)
+
+    def put(self, request, id):
+        document = self.get_object(id)
+        serializer = DocumentSerializer(document, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        document = self.get_object(id)
+        document.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# decorators
 
 
 @api_view(['GET', 'POST'])
