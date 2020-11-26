bc34c7fda20f1da2bc1e3245de0a3a567ee830ab
mnpenchev <mnpenchev@gmail.com>
2020-11-26 11:27:09 +0000

    updated database

--- DocManSys_django_rest/settings.py
+++ DocManSys_django_rest/settings.py
@@ -85,17 +85,13 @@ WSGI_APPLICATION = 'DocManSys_django_rest.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'DocManSysDB',
-        'USER': 'postgres',
-        'PASSWORD': 'password1',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
