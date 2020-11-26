2d41f108908b5f1b3a5283b40a176e883315f652
mnpenchev <mnpenchev@gmail.com>
2020-11-26 11:47:09 +0000

    updated settings

--- DocManSys_django_rest/settings.py
+++ DocManSys_django_rest/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = 'v8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = ['127.0.0.1', 'ec2-35-176-9-84.eu-west-2.compute.amazonaws.com']
 
